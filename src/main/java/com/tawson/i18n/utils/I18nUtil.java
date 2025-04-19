24fdf161ccb3e4023e60efb3296567188d54ab0c
zhengsongtao <shotozheng@163.com>
2025-04-19 18:07:32 +0800

    init

--- src/main/java/com/tawson/i18n/utils/I18nUtil.java
+++ src/main/java/com/tawson/i18n/utils/I18nUtil.java
@@ -58,7 +58,7 @@ public class I18nUtil {
             locale = Locale.US;
         } else {
             try {
-                var split = lang.split(I18nConstants.SEPARATOR);
+                String[] split = lang.split(I18nConstants.SEPARATOR);
                 locale = new Locale(split[0], split[1]);
             } catch (Exception e) {
                 locale = Locale.US;
